4e3610898aba8669cf925e8128427c1b4d8e00d7
rev0lgenX <pushwantrai@gmail.com>
2020-03-06 16:54:38 +0545

    toolbar interaction on list editor

--- app/src/main/java/com/revolgenx/anilib/fragment/base/BaseFragment.kt
+++ app/src/main/java/com/revolgenx/anilib/fragment/base/BaseFragment.kt
@@ -1,6 +1,7 @@
 package com.revolgenx.anilib.fragment.base
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.os.bundleOf
 import com.pranavpandey.android.dynamic.support.fragment.DynamicFragment
 
@@ -14,5 +15,9 @@ abstract class BaseFragment : DynamicFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+    }
+
+    fun invalidateOptionMenu() {
+        (activity as AppCompatActivity).invalidateOptionsMenu()
     }
 }
