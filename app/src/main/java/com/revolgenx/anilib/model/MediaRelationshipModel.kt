c19f3b63ec7c0734799c9df684f0bacb14afca08
rev0lgenX <pushwantrai@gmail.com>
2020-03-10 21:51:28 +0545

    some changes in overview

---
+++ app/src/main/java/com/revolgenx/anilib/model/MediaRelationshipModel.kt
@@ -0,0 +1,10 @@
+package com.revolgenx.anilib.model
+
+import com.revolgenx.anilib.type.MediaFormat
+
+class MediaRelationshipModel : BaseMediaModel() {
+    var relationshipType: Int? = null
+    var title: TitleModel? = null
+    var format: Int? = MediaFormat.`$UNKNOWN`?.ordinal
+    var state: Int? = null
+}
---
+++ app/src/main/java/com/revolgenx/anilib/model/MediaStudioModel.kt
@@ -0,0 +1,7 @@
+package com.revolgenx.anilib.model
+
+class MediaStudioModel {
+    var studioId: Int = -1
+    var name: String? = null
+    var isAnimationStudio = false
+}
