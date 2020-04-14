065fae788b6344406c728679647843b2450d03f0
rev0lgenX <pushwantrai@gmail.com>
2020-04-14 21:07:13 +0545

    updated markwon plugin

--- app/src/main/java/com/revolgenx/anilib/plugins/ImageClickHandlerPlugin.kt
+++ app/src/main/java/com/revolgenx/anilib/plugins/ImageClickHandlerPlugin.kt
@@ -1,5 +1,6 @@
 package com.revolgenx.anilib.plugins
 
+import android.graphics.drawable.LayerDrawable
 import android.text.Spanned
 import android.text.style.ClickableSpan
 import android.view.View
@@ -17,6 +18,7 @@ import com.revolgenx.anilib.meta.YoutubeMeta
 import io.noties.markwon.AbstractMarkwonPlugin
 import io.noties.markwon.MarkwonConfiguration
 import io.noties.markwon.MarkwonSpansFactory
+import io.noties.markwon.image.AsyncDrawableSpan
 import io.noties.markwon.image.ImageProps
 import org.commonmark.node.Image
 import timber.log.Timber
@@ -29,11 +31,12 @@ class ImageClickHandlerPlugin : AbstractMarkwonPlugin() {
             builder.setFactory(
                 Image::class.java
             ) { configuration, props ->
+                val span = origin.getSpans(
+                    configuration,
+                    props
+                )
                 arrayOf( // use origin image span
-                    origin.getSpans(
-                        configuration,
-                        props
-                    ),
+                    span,
                     object : ClickableSpan() {
                         override fun onClick(widget: View) {
                             val tv = widget as TextView
@@ -42,6 +45,10 @@ class ImageClickHandlerPlugin : AbstractMarkwonPlugin() {
                             val start = spanned.getSpanStart(this)
                             val end = spanned.getSpanEnd(this)
                             var source = text.substring(start, end)
+
+                            val asyncDrawableSpan = span as AsyncDrawableSpan
+                            asyncDrawableSpan.drawable.result  = (asyncDrawableSpan.drawable.result as LayerDrawable).getDrawable(0)
+
                             when {
                                 source.contains(VIDEO_TAG) -> {
                                     source = source.substring(VIDEO_START_INDEX)
