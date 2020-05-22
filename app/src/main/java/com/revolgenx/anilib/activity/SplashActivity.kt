fee12e092e29c2272b02584441af21248f88ce66
rev0lgenX <pushwantrai@gmail.com>
2020-05-22 15:33:58 +0545

    added splash screen

---
+++ app/src/main/java/com/revolgenx/anilib/activity/SplashActivity.kt
@@ -0,0 +1,81 @@
+package com.revolgenx.anilib.activity
+
+import android.content.Intent
+import android.graphics.drawable.Animatable
+import android.view.View
+import androidx.annotation.StyleRes
+import androidx.appcompat.widget.AppCompatImageView
+import com.pranavpandey.android.dynamic.support.splash.DynamicSplashActivity
+import com.revolgenx.anilib.R
+import com.revolgenx.anilib.controller.AppController
+import com.revolgenx.anilib.controller.ThemeController
+import java.util.*
+
+/**
+ * Implementing a splash screen by using [DynamicSplashActivity].
+ */
+class SplashActivity : DynamicSplashActivity() {
+
+    /**
+     * Splash image view to start animations.
+     */
+    private var mSplash: AppCompatImageView? = null
+
+    override fun getLocale(): Locale? {
+        // TODO: Not implementing multiple locales so, returning null.
+        return null
+    }
+
+    @StyleRes
+    override fun getThemeRes(): Int {
+        // Return activity theme to be applied.
+        return ThemeController.appStyle
+    }
+
+    override fun onCustomiseTheme() {
+        // Customise activity theme after applying the base style.
+        ThemeController.setLocalTheme()
+    }
+
+    override fun setNavigationBarTheme(): Boolean {
+        // TODO: Return true to apply the navigation bar theme.
+        return AppController.instance.isThemeNavigationBar
+    }
+
+    override fun setNavigationBarThemeInLandscape(): Boolean {
+        return true
+    }
+
+    override fun getLayoutRes(): Int {
+        return R.layout.activity_splash
+    }
+
+    override fun getMinSplashTime(): Long {
+        return 550
+    }
+
+    override fun onViewCreated(view: View) {
+        mSplash = view.findViewById(R.id.splash_image)
+    }
+
+    override fun onPreSplash() {
+//        if (mSplash != null) {
+//            (mSplash!!.drawable as Animatable).start()
+//        }
+    }
+
+    override fun doBehindSplash() {
+        // TODO: Do any operation behind the splash.
+    }
+
+    override fun onPostSplash() {
+        // TODO: Do any operation on post splash.
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+    }
+}
