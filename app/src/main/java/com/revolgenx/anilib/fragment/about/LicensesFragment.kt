224064dff5a6489bbc4d655043603ab4afbcb8c4
rev0lgenX <pushwantrai@gmail.com>
2020-05-25 21:07:25 +0545

    updated license list

---
+++ app/src/main/java/com/revolgenx/anilib/fragment/about/LicensesFragment.kt
@@ -0,0 +1,8 @@
+package com.revolgenx.anilib.fragment.about
+
+import com.revolgenx.anilib.R
+import com.revolgenx.anilib.fragment.base.BaseLayoutFragment
+
+class LicensesFragment :BaseLayoutFragment(){
+    override val layoutRes: Int = R.layout.license_fragment_layout
+}
