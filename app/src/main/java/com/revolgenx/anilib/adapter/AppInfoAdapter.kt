c1698ca407d39ad2f547c448d29dda0f4001bdfe
rev0lgenX <pushwantrai@gmail.com>
2020-05-31 08:56:07 +0545

    updated about section

---
+++ app/src/main/java/com/revolgenx/anilib/adapter/AppInfoAdapter.kt
@@ -0,0 +1,29 @@
+package com.revolgenx.anilib.adapter
+
+import android.view.View
+import com.pranavpandey.android.dynamic.support.recyclerview.adapter.DynamicSimpleBinderAdapter
+import com.pranavpandey.android.dynamic.support.recyclerview.binder.DynamicRecyclerViewBinder
+import com.revolgenx.anilib.adapter.binder.AppInfoBinder
+import com.revolgenx.anilib.adapter.binder.PatreonInfoBinder
+import com.revolgenx.anilib.adapter.binder.PrivacyPolicyBinder
+
+class AppInfoAdapter : DynamicSimpleBinderAdapter<DynamicRecyclerViewBinder<*>>() {
+
+    var onClickListener: View.OnClickListener? = null
+        set(onClickListener) {
+            field = onClickListener
+
+            if (!isComputingLayout) {
+                notifyDataSetChanged()
+            }
+        }
+
+    init {
+        addDataBinders(
+            AppInfoBinder(this),
+            PatreonInfoBinder(this),
+            PrivacyPolicyBinder(this)
+        )
+    }
+
+}
---
+++ app/src/main/java/com/revolgenx/anilib/adapter/binder/AppInfoBinder.kt
@@ -0,0 +1,45 @@
+package com.revolgenx.anilib.adapter.binder
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.pranavpandey.android.dynamic.support.recyclerview.binder.DynamicRecyclerViewBinder
+import com.pranavpandey.android.dynamic.support.utils.DynamicLayoutUtils
+import com.pranavpandey.android.dynamic.support.view.DynamicInfoView
+import com.revolgenx.anilib.R
+import com.revolgenx.anilib.adapter.AppInfoAdapter
+
+
+class AppInfoBinder(binderAdapter: AppInfoAdapter) : DynamicRecyclerViewBinder<AppInfoBinder.ViewHolder>(binderAdapter) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.layout_info_app, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+    /**
+     * Holder class to hold view holder item.
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val dynamicInfoView: DynamicInfoView = view.findViewById(R.id.info_app)
+
+        init {
+            dynamicInfoView.linksView.layoutManager =
+                DynamicLayoutUtils.getGridLayoutManager(
+                    dynamicInfoView.context,
+                    DynamicLayoutUtils.getGridCount(dynamicInfoView.context)
+                )
+        }
+    }
+}
